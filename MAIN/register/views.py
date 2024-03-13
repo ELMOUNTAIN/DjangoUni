e26f22ba90645178d8eca1f478dd5408cd8edb62
ELMOUNTAIN <swashy365@gmail.com>
2024-03-13 13:12:09 +0000

    added content to  register/views.py and signup.html

--- MAIN/register/views.py
+++ MAIN/register/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def signup(request):
     context={}
     form = UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            new_user = form.save
+            login(request, new_user)
+            return redirect("home")
+    context.update({
+        "form":form,
+        "title": "Signup",
+    })
     return render(request, 'register/signup.html', context)
